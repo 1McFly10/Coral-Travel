508ca6ceeebed0f94d92f925f6c4c668433ab4d6
1McFly10 <vlados3702@gmail.com>
2020-09-05 15:20:51 +0300

    ready adaptive main and tours pages

--- scripts/header.js
+++ scripts/header.js
@@ -3,11 +3,13 @@ $('.menu-btn').on('click', function(e) {
     $('.left_sidebar').toggleClass('menu_active');
     $('.main_content').toggleClass('content_active');
     $('.menu-btn').css("display","none");
+    $('.left_sidebar').css('overflowX','hidden')
 })
 $('.cancel-btn').on('click', function(e) {
     e.preventDefault();
     $('.left_sidebar').toggleClass('menu_active');
     $('.main_content').toggleClass('content_active');
+    $('.left_sidebar').css('overflowX','inherit')
     setTimeout(()=>{
         $('.menu-btn').css("display","flex");
     }, 280)
